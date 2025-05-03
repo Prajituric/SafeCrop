5808b0a80c5cbcf3a4e8e8e72f2ccfd1ab64486b
Prajituric <bugheanudanutandrei@gmail.com>
2025-05-03 21:01:01 +0300

    Commit New Changes

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-const yearElm = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearElm.textContent = currentYear;
-
+// Select necessary elements
 const btnNavElm = document.querySelector(".btn-mobile-nav");
 const headerElm = document.querySelector(".header");
+
+// Toggle nav open class on button click
 btnNavElm.addEventListener("click", function () {
   headerElm.classList.toggle("nav-open");
 });
 
+// Smooth scrolling for anchor links
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    if (href === "#")
+    if (href === "#") {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    }
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
+    // Close mobile nav after clicking a link
     if (link.classList.contains("main-nav-link"))
       headerElm.classList.toggle("nav-open");
   });
 });
 
+// Sticky Header for Hero Section
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -51,6 +53,7 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionHeroEl);
 
+// Check if Flexbox Gap is supported
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
@@ -63,8 +66,12 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
+
+// Display the current year
+const yearElm = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearElm.textContent = currentYear;
